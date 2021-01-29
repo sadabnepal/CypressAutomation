83b9fc3347a409646236530624d25533bb247a78
saqibm <MohammadSadab.Saqib@anz.com>
2021-01-30 01:04:56 +0530

    implemented page object pattern

--- cypress/integration/examples/herokuApp.spec.js
+++ cypress/integration/examples/herokuApp.spec.js
@@ -1,50 +1,49 @@
-describe('Validate all basic funcationality of Internet herokuapp', ()=> 
-{
+import HerokuAppPage from "../Pages/HerokuAppPage" 
+
+describe('Validate all basic funcationality of Internet herokuapp', ()=>  {
     it('Open the herokuapp and validate title', ()=> {
-        cy.visit('http://the-internet.herokuapp.com/');  //open url
+        HerokuAppPage.visit();
         cy.title().should('eq', 'The Internet');  //validate page title
     })
 
     it('Validate header of the page', ()=> {
-        cy.get('.heading').should('be.visible'); //check if header is visible
+        HerokuAppPage.pageHeader.should('be.visible'); //check if header is visible
     })
 
     it('Validate form authentication', ()=> {
-        cy.get("#content>ul>li>a[href='/login']").click();  //click on login link
-        cy.get('#username').type("tomsmith"); //enter username
-        cy.get('#password').type("SuperSecretPassword!");  //enter password
-        cy.get('[type=submit]').click(); //click on submit button
-        cy.wait(2000);
-        cy.get('.flash').should('contain', 'You logged into a secure area!');  //validate login success message
-        cy.get("[href='/logout']").click();  //logout
-        cy.wait(2000);
-        cy.get('.flash').should('contain', 'You logged out of the secure area!'); //validate logout message
+        HerokuAppPage.clickOnFormAuthenticationMenu();  //click on login link
+        HerokuAppPage.UserNameTextbox.type("tomsmith"); //enter username
+        HerokuAppPage.PasswordTextbox.type("SuperSecretPassword!");  //enter password
+        HerokuAppPage.SubmitBtn.click(); //click on submit button
+        HerokuAppPage.Flash.should('contain', 'You logged into a secure area!');  //validate login success message
+        HerokuAppPage.LogoutBtn.click();  //logout
+        HerokuAppPage.Flash.should('contain', 'You logged out of the secure area!'); //validate logout message
     })
 
     it('Validate alert popup', ()=> {
         cy.visit('http://the-internet.herokuapp.com/javascript_alerts'); //open alert page
-        cy.get("[onClick='jsAlert()']").click(); //click on alert button
+        HerokuAppPage.JSalertBtn.click(); //click on alert button
 
         //Validating alert message
         cy.on('window:alert', (str) => {
             expect(str).to.equal('I am a JS Alert');
         });
-        cy.get('#result').contains('You successfuly clicked an alert');
+        HerokuAppPage.AlertResult.contains('You successfuly clicked an alert');
     })
 
     it('Validate confirmation popup', ()=> {
-        cy.get("[onclick='jsConfirm()']").click();  //click on confirmJS popup button
+        HerokuAppPage.JSalertCofirmBtn.click();  //click on confirmJS popup button
         //Validating confirmation popup message
         cy.on('window:confirm', (str)=> {
             expect(str).to.eq('I am a JS Confirm');
         });
-        cy.get('#result').contains('You clicked: Ok');  //validating text message displayed for action 
+        HerokuAppPage.AlertResult.contains('You clicked: Ok');  //validating text message displayed for action 
     })
 
     it('Validate page navigation', ()=> {
       cy.visit('http://the-internet.herokuapp.com/');   //open herokuapp homepage
       cy.url().should('eq', 'http://the-internet.herokuapp.com/');
-      cy.get("#content>ul>li>a[href='/checkboxes']").click();   //redirect to Checkbox page
+      HerokuAppPage.CheckBoxMenuItem.click();   //redirect to Checkbox page
       cy.url().should('eq', 'http://the-internet.herokuapp.com/checkboxes');
 
       cy.go('back'); //back to homepage
@@ -56,20 +55,20 @@ describe('Validate all basic funcationality of Internet herokuapp', ()=>
     })
 
     it('Validate webtable', ()=> {
-        cy.visit('http://the-internet.herokuapp.com/');
-        cy.get("#content>ul>li>a[href='/tables']").click();   //click on sortable link
+        HerokuAppPage.visit();
+        HerokuAppPage.TableMenuItem.click();   //click on sortable link
 
         //validate value presence in any td
-        cy.get('#table2').contains('td', 'fbach@yahoo.com').should('be.visible');
+        HerokuAppPage.Table2.contains('td', 'fbach@yahoo.com').should('be.visible');
 
         //validate value presence in particular row and column
-        cy.get('#table2>tbody>tr:nth-child(1)>td:nth-child(3)').contains('jsmith@gmail.com').should('be.visible');
+        HerokuAppPage.Table2Email1stItem.contains('jsmith@gmail.com').should('be.visible');
 
         //find website based on last name of the table of 'http://the-internet.herokuapp.com/tables'
-        cy.get('#table2>tbody>tr td.last-name').each(($lname, index, $list) => {          
+        HerokuAppPage.Table2LastNames.each(($lname, index, $list) => {          
             const lastname = $lname.text();
             if(lastname.includes('Doe')) {
-                cy.get('#table2>tbody>tr td:nth-child(5)').eq(index).then(function(webname){
+                HerokuAppPage.Table2Websites.eq(index).then(function(webname){
                     const websitename = webname.text();
                     expect(websitename).to.equals('http://www.jdoe.com');
                 })
